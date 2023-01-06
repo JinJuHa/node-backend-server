218042ff74185fa37844f0c289fdefec02bb33f6
JinJuHa <jooha321@gmail.com>
2023-01-06 18:27:20 +0900

    get

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true})) 
 
